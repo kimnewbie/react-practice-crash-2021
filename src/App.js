98f0fda43194588878aacdeb0d7e37ac9b93a41f
kimnewbie <gracecancho@gmail.com>
2021-08-16 16:01:48 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import About from './components/About';
 import AddTask from './components/AddTask';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
@@ -9,8 +12,8 @@ const App = () => {
 
     useEffect(() => {
         const getTasks = async () => {
-          const tasksFromServer = await fetchTasks()
-          setTasks(tasksFromServer)
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
         }
         getTasks()
     }, [])
@@ -23,49 +26,110 @@ const App = () => {
         // console.log(data)
         return data
     }
+
+    // Fetch Task
+    const fetchTask = async (id) => {
+        const res = await fetch(`http://localhost:5000/tasks/${id}`)
+        const data = await res.json()
+
+        // console.log(data)
+        return data
+    }
+
     // Add Task
-    const addTask = (task) => {
-        // console.log(task)
-        const id = Math.floor(Math.random() * 10000) + 1
-        // console.log(id)
-        const newTask = {
-            id,
-            ...task
-        }
+    const addTask = async (task) => {
+        // server 연결! db.json에 실제로 추가
+        const res = await fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+        const data = await res.json()
+
         setTasks([
             ...tasks,
-            newTask
+            data
         ])
+        /*
+        // 로컬 부분
+        const id = Math.floor(Math.random() * 10000) + 1
+        const newTask = { id, ...task }
+        setTasks([ ...tasks, newTask ])
+        */
     }
 
     // Delete Task
-    const deleteTask = (id) => {      
-        // console.log('delete', id)
+    const deleteTask = async (id) => {
+        // 서버 넣은 뒤, 수정 부분. 진짜 지워지는 부분
+        await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+        //console.log('delete', id)
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
     // Toggle Reminder
-    const toggleReminder = (id) => {
-        // console.log(id)
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id)
+        const updTask = {
+            ...taskToToggle,
+            reminder: !taskToToggle.reminder
+        }
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updTask)
+        })
+
+        const data = await res.json()
+
         setTasks(tasks.map(
             (task) => task.id === id
                 ? {
                     ...task,
-                    reminder: !task.reminder
+                    reminder: data.reminder
                 }
                 : task
         ))
+        /*
+        // Local
+        setTasks(tasks.map(
+            (task) => task.id === id
+                ? { ...task, reminder: !task.reminder } : task
+        ))
+        */
     }
 
     return (
-        <div className="container">
-            <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/> {showAddTask && <AddTask onAdd={addTask}/>}
-            {
-                tasks.length > 0
-                    ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>)
-                    : ('No Tasks To Show')
-            }
-        </div>
+        <Router>
+
+            <div className="container">
+                <Route
+                    path='/'
+                    exact="exact"
+                    render={(props) => (
+                      <> 
+                        <Header 
+                          onAdd = { () => setShowAddTask(!showAddTask) }
+                          showAdd = { showAddTask } 
+                        /> 
+                        {
+                            showAddTask && <AddTask onAdd={addTask}/>
+                        } 
+                        {
+                            tasks.length > 0
+                                ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>)
+                                : ('No Tasks To Show')
+                        } 
+                      </>
+                    )}/>
+                <Route path='/about' component={About}/>
+                <Footer/>
+            </div>
+        </Router>
     );
 }
 
